3ee84bc58d6dc71e443f274a59189d00ceb70ffd
thehilmisu <ozcanhakanzi@mgmail.com>
2024-11-04 19:36:31 +0100

    minor improvements

--- quizbackend/src/main/java/com/softcrafting/quizbackend/dao/QuizDao.java
+++ quizbackend/src/main/java/com/softcrafting/quizbackend/dao/QuizDao.java
@@ -2,6 +2,8 @@ package com.softcrafting.quizbackend.dao;
 
 import com.softcrafting.quizbackend.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuizDao extends JpaRepository<Quiz, Integer> {
 }
